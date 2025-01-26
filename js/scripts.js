2ee3847550316883928cea0054ee28e7a768be08
Chibron <axel.gibo.pro@gmail.com>
2025-01-26 22:59:40 +0100

    updated js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-pokemonList = [{ name: 'Pikachu', type: 'electric', height: 45 },
-    { name: 'Mewtwo', type: 'psychic', height: 165 },
-    { name: 'Kicklee', type: 'fighting', height: 140},
-]
+let pokemonList = [
+    { name: "Pikachu", types: ["electric"], height: 45 },
+    { name: "Mewtwo", types: ["psychic"], height: 165 },
+    { name: "Kicklee", types: ["fighting"], height: 140 },
+    { name: 'Charizard', types: ['fire', 'flying'], height: 175 },
+];
+
+// Loop through the Pokémon array
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+
+    document.write('<div class="pokemon-card">');
+
+    document.write("<h2>" + pokemon.name + " (type: " + pokemon.types.join(", ") + ")</h2>");
+    document.write("<p>Height: " + pokemon.height + "</p>");
+
+    if (pokemon.height > 170) {
+        document.write("<p>Wow, that's big!</p>");
+    }
+
+    document.write('</div>');
+}
