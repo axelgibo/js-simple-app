26a3cce65b06e8947bde03af248f721dd98deb6f
Chibron <axel.gibo.pro@gmail.com>
2025-01-28 21:13:22 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
 let pokemonList = [
-    { name: "Pikachu", types: ["electric"], height: 45 },
-    { name: "Mewtwo", types: ["psychic"], height: 165 },
-    { name: "Kicklee", types: ["fighting"], height: 140 },
-    { name: 'Charizard', types: ['fire', 'flying'], height: 175 },
+  { name: "Pikachu", types: ["electric"], height: 45 },
+  { name: "Mewtwo", types: ["psychic"], height: 165 },
+  { name: "Kicklee", types: ["fighting"], height: 140 },
+  { name: "Charizard", types: ["fire", "flying"], height: 175 },
 ];
 
-// Loop through the Pokémon array
-for (let i = 0; i < pokemonList.length; i++) {
+// printArrayDetails function declaration
+function printArrayDetails(list) {
+  for (let i = 0; i < pokemonList.length; i++) {
     let pokemon = pokemonList[i];
 
     document.write('<div class="pokemon-card">');
 
-    document.write("<h2>" + pokemon.name + " (type: " + pokemon.types.join(", ") + ")</h2>");
+    document.write(
+      "<h2>" + pokemon.name + " (type: " + pokemon.types.join(", ") + ")</h2>"
+    );
     document.write("<p>Height: " + pokemon.height + "</p>");
 
     if (pokemon.height > 170) {
-        document.write("<p>Wow, that's big!</p>");
+      document.write("<p>Wow, that's big!</p>");
     }
 
-    document.write('</div>');
+    document.write("</div>");
+  }
+}
+
+printArrayDetails(pokemonList);
+
+printArrayDetails(pokemonList2);
+
+function divide(dividend, divisor) {
+  if (divisor === 0) {
+    return "You are trying to divide by 0";
+  } else {
+    let result = dividend / divisor;
+    return result;
+  }
 }
