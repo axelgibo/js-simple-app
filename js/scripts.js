31cadce34068a1e91d35646fecc8997b637e4d3b
Chibron <axel.gibo.pro@gmail.com>
2025-02-01 16:04:56 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,38 @@
-let pokemonList = [
-  { name: "Pikachu", types: ["electric"], height: 45 },
-  { name: "Mewtwo", types: ["psychic"], height: 165 },
-  { name: "Kicklee", types: ["fighting"], height: 140 },
-  { name: "Charizard", types: ["fire", "flying"], height: 175 },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Pikachu", types: ["electric"], height: 45 },
+    { name: "Mewtwo", types: ["psychic"], height: 165 },
+    { name: "Kicklee", types: ["fighting"], height: 140 },
+    { name: "Charizard", types: ["fire", "flying"], height: 175 },
+  ];
 
-// printArrayDetails function declaration
-function printArrayDetails(list) {
-  list.forEach(function (pokemon) {
-    document.write('<div class="pokemon-card">');
+  function getAll() {
+    return pokemonList;
+  }
 
-    document.write(
-      "<h2>" + pokemon.name + " (type: " + pokemon.types.join(", ") + ")</h2>"
-    );
-    document.write("<p>Height: " + pokemon.height + "</p>");
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-    if (pokemon.height > 170) {
-      document.write("<p>Wow, that's big!</p>");
-    }
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
 
-    document.write("</div>");
-  });
-}
+pokemonRepository.getAll().forEach(function (pokemon) {
+  document.write('<div class="pokemon-card">');
+  document.write(
+    "<h2>" + pokemon.name + " (type: " + pokemon.types.join(", ") + ")</h2>"
+  );
+  document.write("<p>Height: " + pokemon.height + "</p>");
+
+  if (pokemon.height > 170) {
+    document.write("<p>Wow, that's big!</p>");
+  }
+
+  document.write("</div>");
+});
 
 // Call the function to print the details
 printArrayDetails(pokemonList);
